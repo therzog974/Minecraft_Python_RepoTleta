eec693a8dff66238581bc5825337d5bf66a4c707
jlopez249 <jlopez249@lsr7.net>
2017-11-21 13:15:07 -0600

    Booglin

---
+++ notAir.py
@@ -0,0 +1,9 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+pos = mc.player.getPos()
+x = pos.x
+y = pos.y
+z = pos.z
+blockType = mc.getBlock(x,y,z)
+notAir = blockType == 0
+mc.postToChat("The player isn't standing in air" + str( notAir))
